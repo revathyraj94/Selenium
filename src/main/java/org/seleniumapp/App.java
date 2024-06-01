c17ea1dc99e4ef6a55dda0b1f327bc4e67bb0a11
RahulDevan <inform.rahuldevan@gmail.com>
2024-06-01 13:43:19 -0400

    committing maven project changes

---
+++ src/main/java/org/seleniumapp/App.java
@@ -0,0 +1,13 @@
+package main.java.org.seleniumapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
